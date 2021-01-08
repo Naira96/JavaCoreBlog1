d6730f1654a662f4109a89bad7e04a2ae5acf33b
Nina <shukhyann@mail.ru>
2021-01-08 20:39:18 +0400

    Comparator example

--- src/classExample/compare/UserNameComparator.java
+++ src/classExample/compare/UserNameComparator.java
@@ -2,7 +2,7 @@ package classExample.compare;
 
 import java.util.Comparator;
 
-public class UserTitleComparator implements Comparator<User> {
+public class UserNameComparator implements Comparator<User> {
     @Override
     public int compare(User o1, User o2) {
         return o1.getName().compareTo(o2.getName());
